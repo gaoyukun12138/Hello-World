e3ce7361950a4f7741ca75976001b3141887d5e2
gaoyukun12138 <2898604753@qq.com>
2025-04-28 20:04:38 +0800

    “创建main.cpp文件，写了初始的代码”

---
+++ main.cpp
@@ -0,0 +1,16 @@
+
+#inlcude <iostream>
+
+using namespace std;
+
+void sort(int arr[], int size)
+{
+}
+
+int main()
+{
+	int arr[] = {12, 4, 89, 43, 21, 78};
+	int size = sizeof(arr)/sizeof(srr[0]);
+	sort(arr, size);
+	return 0;
+}
